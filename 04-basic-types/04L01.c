6feb2873e58f486b363a6bade01d2f5b6538b7d1
Xiaoli Shen <alabebop@gmail.com>
2021-01-09 01:16:47 +0900

    04L01: char

---
+++ 04-basic-types/04L01.c
@@ -0,0 +1,20 @@
+/* 04L01.c: Printing characters*/
+#include <stdio.h>
+
+int main()
+{
+    // char c1;
+    // char c2;
+
+    // c1 = 'A';
+    // c2 = 'a';
+    int c1 = 'A', c2 = 97;
+
+    printf("Print c1 as a charater: %c.\n", c1);
+    printf("Print c1 as a digit: %d.\n", c1);
+    printf("Print c2 as a charater: %c.\n", c2);
+    printf("Print c2 as a digit: %d.\n", c2);
+    printf("Print 2580 as a character: %c.\n", 2580);
+
+    return 0;
+}
