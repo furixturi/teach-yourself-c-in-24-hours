e8f9e22cc1d438e2945f169028f2ebd4c5650ee5
Xiaoli Shen <alabebop@gmail.com>
2021-01-09 14:40:58 +0900

    hour 5 decimal to hex uppercase and hex lowercase

---
+++ 05-io/05L02.c
@@ -0,0 +1,8 @@
+/* 05L02.c: convert decimal to hex and hex uppercase with printf */
+#include <stdio.h>
+
+int main()
+{
+    printf("Decimal\tHEX\thex\n%d\t%X\t%x\n", 15, 15, 15);
+    return 0;
+}
